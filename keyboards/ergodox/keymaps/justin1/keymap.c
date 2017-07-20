4986c98b675891420b1feee5eb7f171acb418923
justin999 <koichi.sato.aow@gmail.com>
2017-07-20 09:58:30 +0900

    update keymaps

--- keyboards/ergodox/keymaps/justin1/keymap.c
+++ keyboards/ergodox/keymaps/justin1/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        | Copy |PASTE |       |   `  | ESC  |
  *                                 ,------|------+------|       |------+------+------.
- *                                 |      |      |  [   |       |  PgUp|      |      |
+ *                                 |      |      |ScCopy|       |  PgUp|      |      |
  *                                 |Space |' = ' |------|       |------| Space| Enter|
  *                                 |      |      | Spot |       |  PgDn|      |      |
  *                                 `--------------------'       `--------------------'
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,    KC_LBRC,
 	KC_LCTL, LT(1,KC_A),  KC_S, KC_D,     KC_F,     KC_G,
 	KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_B,
-	LT(1,KC_GRAVE),   LGUI(KC_C),  LGUI(KC_V), KC_LALT,  KC_LGUI,
+	KC_F7,   LGUI(KC_C),  LGUI(KC_V), KC_LALT,  KC_LGUI,
 	LGUI(KC_C),  LGUI(KC_V),
-	KC_LPRN,
+	SCMD(LCTL(KC_4)),
 	KC_SPC, KC_EQL,  LALT(KC_SPC),
   
 	// Right hand
