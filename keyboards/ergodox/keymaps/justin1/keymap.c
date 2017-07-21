29c6fa68c60ba5e6df8b95421529954a28a0471d
justin999 <koichi.sato.aow@gmail.com>
2017-07-21 17:08:06 +0900

    update keymap

--- keyboards/ergodox/keymaps/justin1/keymap.c
+++ keyboards/ergodox/keymaps/justin1/keymap.c
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctl    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|  B   |           |  B   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * |( LShift|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |RShift )|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | L1   |  Copy| PASTE| Alt  | LGui |                                       | RGUI | Left |  Dn  |  Up  | Right  | 
+ *   | Spot |  Copy| PASTE| Alt  | LGui |                                       | RGUI | Left |  Dn  |  Up  | Right  | 
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Copy |PASTE |       |   `  | ESC  |
@@ -47,17 +47,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_GESC,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,    KC_GRV,
 	KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,    KC_LBRC,
 	KC_LCTL, LT(1,KC_A),  KC_S, KC_D,     KC_F,     KC_G,
-	KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_B,
-	KC_F7,   LGUI(KC_C),  LGUI(KC_V), KC_LALT,  KC_LGUI,
+	KC_LSPO,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_B,
+	LALT(KC_SPC),LGUI(KC_C),LGUI(KC_V), KC_LALT,  KC_LGUI,
 	LGUI(KC_C),  LGUI(KC_V),
 	SCMD(LCTL(KC_4)),
-	KC_SPC, KC_EQL,  LALT(KC_SPC),
+	KC_SPC, KC_EQL,  LALT(KC_S),
   
 	// Right hand
 	KC_6,    KC_7,    KC_8,     KC_9,     KC_0,    KC_MINS,   KC_BSPC,
 	KC_RBRC,  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,    KC_BSLS,
 	          KC_H,    KC_J,    KC_K,     KC_L,     LT(1,KC_SCOLON), KC_QUOT,
-	KC_B ,  KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSFT,
+	KC_B ,  KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_RSPC,
 	KC_RGUI,KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT,
 	KC_GRV,  KC_ESC,
 	KC_PGUP,
@@ -67,9 +67,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |  Esc   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | BSpace |
  * |--------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      | MsUp |      |      |      |           |      |      | Mute |VolDn |VolUp |   {  |   }    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |        |
+ * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |   [  |   ]    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      | LClk | MClk | RClk |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	RESET,   KC_TRNS, KC_TRNS,
 	// Right hand
 	KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC,
-	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LSFT(KC_LBRC), LSFT(KC_RBRC),
+	KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, LSFT(KC_LBRC), LSFT(KC_RBRC),
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC,
 	KC_TRNS, M(_MOB), KC_TRNS, M(_CUS1),M(_CUS2),KC_TRNS, KC_TRNS,
 	EM_UNDO, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS,
